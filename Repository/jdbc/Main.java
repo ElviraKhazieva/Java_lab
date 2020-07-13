b9dd72d4a80b6ce129019f2b9e9ff42d6fe93fba
Elvira <hazim219@yandex.ru>
2020-07-13 23:15:22 +0300

    repository is implemented

---
+++ Repository/jdbc/Main.java
@@ -0,0 +1,31 @@
+package jdbc;
+
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import models.Mentor;
+import models.Student;
+import repositories.StudentRepository;
+import repositories.StudentRepositoryJdbcImpl;
+
+import java.sql.*;
+
+public class Main {
+    private static final String URL = "jdbc:postgresql://localhost:5432/java_lab_pract_2020";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Qwer1234";
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        StudentRepository studentRepository = new StudentRepositoryJdbcImpl(connection);
+        System.out.println(studentRepository.findById(2L));
+        System.out.println(studentRepository.findAll());
+        System.out.println(studentRepository.findAllByAge(26));
+        Student s = new Student(3L, "Катя", "Царева", 19, 901);
+        studentRepository.update(s);
+        Student s2 = new Student(null, "Рустем", "Хакимуллин", 19, 901);
+        studentRepository.save(s2);
+        Student s3 = new Student(null, "Максим", "Иванов", 19, 904);
+        studentRepository.save(s3);
+        connection.close();
+    }
+}
---
+++ Repository/models/Subject.java
@@ -0,0 +1,51 @@
+package models;
+
+import java.util.Objects;
+
+public class Subject {
+    private long id;
+    private String name;
+
+    public Subject(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subject subject = (Subject) o;
+        return id == subject.id &&
+                Objects.equals(name, subject.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
