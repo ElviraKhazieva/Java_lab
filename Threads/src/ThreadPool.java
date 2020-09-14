7858943691e590fd8c1c6987e738ff5140e83fe7
Elvira <hazim219@yandex.ru>
2020-09-14 19:15:32 +0300

    Thread Pool is added

---
+++ Threads/src/ThreadPool.java
@@ -0,0 +1,49 @@
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+// wait, notify, synchronized
+public class ThreadPool {
+    // очередь задач
+    private Deque<Runnable> tasks;
+
+    // пул потоков
+    private PoolWorker threads[];
+
+    public ThreadPool(int threadsCount) {
+        this.tasks = new ConcurrentLinkedDeque<>();
+        this.threads = new PoolWorker[threadsCount];
+
+        for (int i = 0; i < this.threads.length; i++) {
+            this.threads[i] = new PoolWorker();
+            this.threads[i].start();
+        }
+    }
+
+    public void submit(Runnable task) {
+        synchronized (tasks) {
+            tasks.add(task);
+            tasks.notify();
+        }
+    }
+
+    // класс - рабочий поток
+    private class PoolWorker extends Thread {
+        @Override
+        public void run() {
+            Runnable r;
+            while (true) {
+                synchronized (tasks) {
+                    while (tasks.isEmpty()) {
+                        try {
+                            tasks.wait();
+                        } catch (InterruptedException e) {
+                            throw new IllegalStateException(e);
+                        }
+                    }
+                    r = tasks.pollFirst();
+                }
+                r.run();
+            }
+        }
+    }
+}
