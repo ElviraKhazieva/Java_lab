d57251806e371c80e7b2c88650a87fd87a5a9608
Elvira <hazim219@yandex.ru>
2020-10-28 21:25:05 +0300

    remove repository

--- Repository/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package repositories;
-
-import models.Student;
-
-import java.util.List;
-
-public interface StudentRepository extends CrudRepository<Student> {
-    List<Student> findAllByAge(int age);
-}
