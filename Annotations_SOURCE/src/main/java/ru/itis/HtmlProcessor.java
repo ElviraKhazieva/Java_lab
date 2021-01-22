ebaa6273ce36bdb92e744e7d903a57de523051cf
Elvira <hazim219@yandex.ru>
2021-01-22 16:57:28 +0300

    some fixes

--- Annotations_SOURCE/src/main/java/ru/itis/HtmlProcessor.java
+++ Annotations_SOURCE/src/main/java/ru/itis/HtmlProcessor.java
@@ -24,6 +24,7 @@ import java.util.*;
 @AutoService(Processor.class)
 @SupportedAnnotationTypes(value = {"ru.itis.annotations.HtmlForm", "ru.itis.annotations.HtmlInput"})
 public class HtmlProcessor extends AbstractProcessor {
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
@@ -34,27 +35,38 @@ public class HtmlProcessor extends AbstractProcessor {
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
-        
-        Set<? extends Element> annotatedInputElements = roundEnv.getElementsAnnotatedWith(HtmlInput.class);
-        Set<? extends Element> annotatedFormElements = roundEnv.getElementsAnnotatedWith(HtmlForm.class);
 
-        for(Element formElement : annotatedFormElements) {
+        Set<? extends Element> annotatedFormElements = roundEnv.getElementsAnnotatedWith(HtmlForm.class);
+        //Set<? extends Element> annotatedInputElements = roundEnv.getElementsAnnotatedWith(HtmlInput.class);
+
+        for(Element formElement: annotatedFormElements) {
 
             String path = HtmlProcessor.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-            //System.out.println(path);
+            System.out.println(path);
             path = path.substring(1) + formElement.getSimpleName().toString() + ".html";
             Path out = Paths.get(path);
             try {
                 Template template = configuration.getTemplate("form.ftlh");
-                HtmlForm annotation = formElement.getAnnotation(HtmlForm.class);
-                Form form = new Form(annotation.action(),annotation.method());
-                List inputs = new ArrayList();
-                for (Element inputElement: annotatedInputElements) {
-                    if(inputElement.getEnclosingElement().equals(formElement)) {
-                        HtmlInput inputAnnotation = inputElement.getAnnotation(HtmlInput.class);
-                        inputs.add(new Input(inputAnnotation.type(),inputAnnotation.name(),inputAnnotation.placeholder()));
+                HtmlForm formAnnotation = formElement.getAnnotation(HtmlForm.class);
+                Form form = new Form(formAnnotation.action(), formAnnotation.method());
+                List<Input> inputs = new ArrayList<>();
+
+//                for (Element inputElement: annotatedInputElements) {
+//                    if(inputElement.getEnclosingElement().equals(formElement)) {
+//                        HtmlInput inputAnnotation = inputElement.getAnnotation(HtmlInput.class);
+//                        inputs.add(new Input(inputAnnotation.type(), inputAnnotation.name(), inputAnnotation.placeholder()));
+//                    }
+//                }
+
+                List<? extends Element> formElementEnclosedElements = formElement.getEnclosedElements();
+                System.out.println(Arrays.toString(formElementEnclosedElements.toArray()));
+                for(Element enclosedElement: formElementEnclosedElements) {
+                    HtmlInput inputAnnotation = enclosedElement.getAnnotation(HtmlInput.class);
+                    if (inputAnnotation != null) {
+                        inputs.add(new Input(inputAnnotation.type(), inputAnnotation.name(), inputAnnotation.placeholder()));
                     }
                 }
+
                 Map<String, Object> attributes = new HashMap<>();
                 attributes.put("form", form);
                 attributes.put("inputs", inputs);
@@ -63,7 +75,9 @@ public class HtmlProcessor extends AbstractProcessor {
             } catch (Exception e) {
                 throw new IllegalStateException(e);
             }
+
         }
         return true;
     }
+
 }
